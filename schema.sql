b3e54af943990a50502b183c3e98142d38fec331
Lans-GB <cyga0916@gmail.com>
2025-11-18 19:14:12 +0800

    Create subjects table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE school_db; USE school_db;
-CREATE subjects(subject_id INT PRIMARY KEY, subject_name VARCHAR(50));
+CREATE TABLE subjects(subject_id INT PRIMARY KEY, subject_name VARCHAR(50));
