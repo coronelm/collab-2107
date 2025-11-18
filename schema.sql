2e581e290cdc81964d4ad1c31738f1c83cf53adb
Sh4wnT0 <24-30563@g.batstate-u.edu.ph>
2025-11-18 16:46:37 +0800

    Allen updated a room number

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE classes(id INT, class_name VARCHAR(50), room_no INT);
 INSERT INTO classes(id, class_name, room_no) VALUES(01,'BSIT-2107',1111), (02,'BSIT-2108',2222), (03,'BSIT-2109',3333);
 ALTER TABLE classes ADD COLUMN teacher_id INT;
 ALTER TABLE classes ADD CONSTRAINT fk_classes_teacher FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id);
+UPDATE classes SET room_no = 4444 WHERE id = 03;
