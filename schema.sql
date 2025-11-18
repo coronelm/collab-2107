85bd4e9efc592c221be7823f192233821fdfb1b4
Kenn <109660753+kennie-29@users.noreply.github.com>
2025-11-18 15:34:16 +0800

    mali po ng insert ma'am sorry po

--- schema.sql
+++ schema.sql
@@ -1,3 +1,1 @@
 CREATE DATABASE school_db; USE school_db;
-CREATE TABLE classes(id INT, class_name VARCHAR(50), room_no INT);
-INSERT INTO classes(id, class_name, room_no) VALUES(01,'BSIT-2107',1111), (02,'BSIT-2108',2222), (03,'BSIT-2109',3333);
