50d2a614ad5c41aa34a7c998c10251ab3485452a
24-39050-ctrl <24-39050@g.batstate-u.edu.ph>
2025-11-19 07:25:52 +0800

    removing

--- schema.sql
+++ schema.sql
@@ -1,8 +1,5 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE users (user_id INT, username VARCHAR(50), role VARCHAR(50));
 INSERT INTO users (user_id, username, role) VALUES (0001, 'admin', 'Admin'), (0002, 'Justin Rey Morota', 'Student');
-git ALTER TABLE users
-ADD COLUMN email VARCHAR(255) NOT NULL UNIQUE;
-git ALTER TABLE users
 ADD COLUMN email VARCHAR(50);
 UPDATE users SET email = 'justinrey@gmail.com' WHERE user_id = 0002;
