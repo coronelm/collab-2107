50686d71c761822d97bf3b83555e957b33b162b0
24-39552 cell- <24-39552@g.batstate-u.edu.ph>
2025-11-18 16:37:08 +0800

    Steven Mercado inserted sample grades

--- schema.sql
+++ schema.sql
@@ -1,1 +1,5 @@
-CREATE DATABASE school_db; USE school_db;
+INSERT INTO Grades_Table (grade_id, students_id, subject_id, score)
+VALUES
+    (1, 2107, 101, 98),
+    (2, 2108, 101, 88),
+    (3, 2109, 101, 74);
