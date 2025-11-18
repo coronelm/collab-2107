809754222676013db44df6dac9d1a390045961d5
M-Coder143 <24-36956@g.batstate-u.edu.ph>
2025-11-18 16:37:26 +0800

    Ken added teacher_id FK column

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE classes(id INT, class_name VARCHAR(50), room_no INT);
 INSERT INTO classes(id, class_name, room_no) VALUES(01,'BSIT-2107',1111), (02,'BSIT-2108',2222), (03,'BSIT-2109',3333);
+ALTER TABLE classes ADD COLUMN teacher_id INT;
+ALTER TABLE classes ADD CONSTRAINT fk_classes_teacher FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id);
