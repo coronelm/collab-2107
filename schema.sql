3f933447f8ad43ac16f215140a07d24f54d6c07b
Justin <justin@gmail.comclearclearclear>
2025-11-18 16:26:55 +0800

    Removed the git word in Insert, shortened insert code

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE users (user_id INT, username VARCHAR(50), role VARCHAR(50));
-git INSERT INTO users (user_id, username, role) VALUES (0001, 'admin', 'Admin');
-git INSERT INTO users (user_id, username, role) VALUES (0002, 'Justin Rey Morota', 'Student');
+INSERT INTO users (user_id, username, role) VALUES (0001, 'admin', 'Admin'), (0002, 'Justin Rey Morota', 'Student');
