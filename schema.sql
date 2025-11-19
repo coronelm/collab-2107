dcb8599066b21c5b76f3a731b18c89de5408c5df
Martin <24-36840@g.batstate-u.edu.ph>
2025-11-19 13:31:18 +0800

    Ubalde added the department table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE teachers (id INT, name VARCHAR(50), subject VARCHAR(50));
 INSERT INTO teachers (id, name, subject) VALUES (101, 'Ms. Coronel', 'DBMS');
 INSERT INTO teachers (id, name, subject) VALUES (102, 'Mr. Tiquio', 'ACP');
 INSERT INTO teachers (id, name, subject) VALUES (103, 'Mr. Balba', 'Physics');
+ALTER TABLE teachers ADD department VARCHAR(50);
