637ff1d7881435e1709cbbf217414db616a16957
johnfrederick <24-35610@g.batstate-u.edu.ph>
2025-11-19 19:36:52 +0800

    Member 4 Bacon of Group 6 Update head teacher name

--- schema.sql
+++ schema.sql
@@ -4,3 +4,4 @@ CREATE TABLE department_table (dept_id INT, dept_name VARCHAR(50));
 INSERT INTO department_table(dept_id, dept_name) VALUES (1, 'CICS);
 INSERT INTO department_table(dept_id, dept_name) VALUES (1, 'CICS'), (2, 'CAS'), (3, 'CET');
 ALTER TABLE department_table ADD COLUMN (head_teacher VARCHAR(50));
+UPDATE department_table SET head_teacher = 'Marielle Coronel' WHERE dept_id = 1;
