aa6c24040ace7a09b2810c39983c7fd0cb331ae2
johnfrederick <24-35610@g.batstate-u.edu.ph>
2025-11-19 18:58:03 +0800

    Member 2 Catibog of Group 6 inserted data in department table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE department_table (dept_id INT, dept_name VARCHAR(50));
 CREATE TABLE department_table (dept_id INT, dept_name VARCHAR(50));
+INSERT INTO department_table(dept_id, dept_name) VALUES (1, 'CICS);
+INSERT INTO department_table(dept_id, dept_name) VALUES (1, 'CICS'), (2, 'CAS'), (3, 'CET');
