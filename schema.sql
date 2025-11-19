5ef3a8da42bf73c65e623881b818fa46359977a2
24-31100-hue <24-31100@g.batstate-u.edu.ph>
2025-11-19 19:16:10 +0800

    Member 3 JOrdan group 6 added head teacher column

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE department_table (dept_id INT, dept_name VARCHAR(50));
 CREATE TABLE department_table (dept_id INT, dept_name VARCHAR(50));
 INSERT INTO department_table(dept_id, dept_name) VALUES (1, 'CICS);
 INSERT INTO department_table(dept_id, dept_name) VALUES (1, 'CICS'), (2, 'CAS'), (3, 'CET');
+ALTER TABLE department_table ADD COLUMN (head_teacher VARCHAR(50));
