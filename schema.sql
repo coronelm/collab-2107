053ccd900dd0dd2e399da23f34b0fb41bc3a8f8c
CJ <cjonera7@gmail.com>
2025-11-19 13:48:52 +0800

    Update student status

--- schema.sql
+++ schema.sql
@@ -4,3 +4,4 @@ INSERT INTO attendance_table (attendance_id, student_id, date, status) VALUES (0
 INSERT INTO attendance_table (attendance_id, student_id, date, status) VALUES (02, 2, '2025-11-19', 'Present);
 INSERT INTO attendance_table (attendance_id, student_id, date, status) VALUES (03, 3, '2025-11-19', 'Present);
 ALTER TABLE attendance_table ADD COLUMN remarks VARCHAR(150);
+UPDATE attendance_table SET status = Absent WHERE student_id = 2;
