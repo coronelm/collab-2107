aa94c8c8c8048428ba61aeb2494b240aabc6f930
cortezlanz19 <24-30368@g.batstate-u.edu.ph>
2025-11-19 11:49:26 +0800

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,2 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE attendance_table(attendance_id INT PK, student_id INT, date DATE, status VARCHAR(50))
-INSERT INTO attendance (attendance_id, student_id, date, status) VALUES (01, 1, '2025-11-19', 'Present');
-INSERT INTO attendance (attendance_id, student_id, date, status) VALUES (02, 2, '2025-11-19', 'Present');
-INSERT INTO attendance (attendance_id, student_id, date, status) VALUES (03, 3, '2025-11-19', 'Present');
