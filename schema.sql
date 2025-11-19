ef6d28ef723dc2660fea7a5636b620c96b648a61
CJ <cjonera7@gmail.com>
2025-11-19 12:09:33 +0800

    Group8 added remarks column

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE attendance_table(attendance_id INT PK, student_id INT, date DATE, s
 INSERT INTO attendance_table (attendance_id, student_id, date, status) VALUES (01, 1, '2025-11-19', 'Present);
 INSERT INTO attendance_table (attendance_id, student_id, date, status) VALUES (02, 2, '2025-11-19', 'Present);
 INSERT INTO attendance_table (attendance_id, student_id, date, status) VALUES (03, 3, '2025-11-19', 'Present);
+ALTER TABLE attendance_table ADD COLUMN remarks VARCHAR(150);
