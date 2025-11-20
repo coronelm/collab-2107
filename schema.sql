352ecefa809527ac547d5687a884ce57a38f12fb
glenntalban <glenntalban068@gmail.com>
2025-11-20 10:22:28 +0800

    Member 2: added sample enrollments

--- schema.sql
+++ schema.sql
@@ -1,1 +1,5 @@
-CREATE TABLE enrollments (enrollment_id INT PRIMARY KEY, student_id INT, class_id INT);
+INSERT INTO enrollments (enrollment_id, student_id, class_id)
+VALUES
+(01, 24-13453, 101),
+(02, 24-87456, 102),
+(03, 24-98742, 103);
