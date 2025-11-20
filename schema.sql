bbe64a76b265b45e49980fa0d41adc24a8347d95
24-38671-del <24-38671@g.batstate-u.edu.ph>
2025-11-20 22:36:00 +0800

    remove duplicate updated department column
    Corrected the name in the update statement for consistency.

--- schema.sql
+++ schema.sql
@@ -4,5 +4,4 @@ INSERT INTO teachers (id, name, subject) VALUES (101, 'Ms. Coronel', 'DBMS');
 INSERT INTO teachers (id, name, subject) VALUES (102, 'Mr. Tiquio', 'ACP');
 INSERT INTO teachers (id, name, subject) VALUES (103, 'Mr. Balba', 'Physics');
 ALTER TABLE teachers ADD department VARCHAR(50);
-UPDATE teachers SET department='CICS' WHERE name='Mr.Tiqiuo';
 UPDATE teachers SET department='CICS' WHERE name='Mr. Tiqiuo';
