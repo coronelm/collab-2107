7f8a09e2332ef997cb4362f30166eca3e83b6ce0
[Cassey] <[24-38605@g.batstate-u.edu.ph]>
2025-11-20 23:14:08 +0800

    Cass; added column age

--- schema.sql
+++ schema.sql
@@ -1,5 +1,2 @@
-INSERT INTO teachers (id, name, subject)
-VALUES
-(1, 'Mr. Dela Cruz', 'math'),
-(2, 'Ms. Santos', 'Science'),
-(3, 'Mr. Ramirez', 'English');
+ALTER TABLE Student_Table
+ADD age INT;
