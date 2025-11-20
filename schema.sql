ed46274bc2ad0e826fb44f5051083061337a74c2
Karl200515 <petallo.karl38@gmail.com>
2025-11-20 11:15:19 +0800

    Fix merge conflict and add school_year column

--- schema.sql
+++ schema.sql
@@ -3,9 +3,7 @@ CREATE TABLE enrollments (
     student_id INT,
     class_id INT
 );
-<<<<<<< HEAD
-ALTER TABLE enrollments ADD school_year int;
-=======
+
 
 INSERT INTO enrollments (enrollment_id, student_id, class_id)
 VALUES
@@ -14,5 +12,4 @@ VALUES
 (03, 24-98742, 103);
 
 
->>>>>>> f35be9e44b6b64ecdaa2e0792e9fd6a2ab96e8a5
 ALTER TABLE enrollments ADD school_year int;
