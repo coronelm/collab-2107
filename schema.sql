4d69cc9d4e5a8e7fcffcc8f5e4f4d9f014182add
Lans-GB <cyga0916@gmail.com>
2025-11-20 11:33:31 +0800

    Group 7 - Silva fixed insert data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE school_db; USE school_db;
+CREATE DATABASE school_db;
 CREATE TABLE subjects(subject_id INT PRIMARY KEY, subject_name VARCHAR(50));
-INSERT INTO subjects (subject_id, subject_name) VALUES (101, Database Management System), (102, Advance Computer Programming);
+INSERT INTO subjects (subject_id, subject_name) VALUES (101, 'Database Management System'), (102, 'Advance Computer Programming');
 ALTER TABLE subjects ADD COLUMN units INT;
 UPDATE subjects SET units = 3 WHERE subject_id = 101;
