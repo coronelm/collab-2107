deeddcc3d3b9281117a5abb7f8eea1d347a9a9a2
glenntalban <glenntalban068@gmail.com>
2025-11-20 10:29:18 +0800

    Member 2: added sample enrollments

--- schema.sql
+++ schema.sql
@@ -3,3 +3,8 @@ VALUES
 (01, 24-13453, 101),
 (02, 24-87456, 102),
 (03, 24-98742, 103);
+INSERT INTO enrollments (enrollment_id, student_id, class_id)
+VALUES
+(01, 24-13453, 101),
+(02, 24-87456, 102),
+(03, 24-98742, 103);
